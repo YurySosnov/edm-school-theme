2cfeb20a6453d21b412188895007a00421222fd3
yurysosnov <yury.v.sosnov@yandex.ru>
2019-12-01 23:24:06 +0300

    DJ Intro page started (Диджеинг)

---
+++ page-dj-intro.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package EDM_School
+ */
+
+get_header('homepage');
+get_template_part( 'template-parts/content', 'homepage' );
+get_footer( 'homepage' );
